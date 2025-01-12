1fc886b5fa096ba231b16a3a22f1b2afb1be498d
Rohan Khalate <rohan@Rohans-MacBook-Air.local>
2025-01-13 01:01:48 +0530

    order.jsx completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ const App = () => {
  return(
   <div>
     <h1>Padre Gino's - Order Now</h1>
-    <pizza name="pepperoni" description="pep,cheese,n stuff"/>
-    <pizza name="Hawaiian" description="ham, pinapple,n stuff"/>
-    <pizza name="Pineapple" description="french fries, hot dogs, n stuff"/>
+    <Pizza name="pepperoni" description="pep,cheese,n stuff" image={"/public/pizzas/pepperoni.webp"}/>
+    <Pizza name="Hawaiian" description="ham, pinapple,n stuff"image={"/public/pizzas/hawaiian.webp"}/>
+    <Pizza name="Pineapple" description="french fries, hot dogs, n stuff"image={"/public/pizzas/big_meat.webp"}/>
   </div>
  );
 };
